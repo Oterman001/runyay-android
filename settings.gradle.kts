0d004efae3ec3bcf5349a3e08b03b43181a1b64e
tianjun <tianjun@oppo.com>
2026-01-15 21:31:53 +0800

    添加内网maven仓库，使得编译通过

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,33 @@
 pluginManagement {
     repositories {
+        maven {
+            //允许使用不安全的maven，gradle7要求必须使用https，内网maven评估不使用https
+            isAllowInsecureProtocol = true
+            //need use oppo maven,due to google repo is not available in services
+            url = uri(providers.gradleProperty("prop_oppoMavenUrlRelease").get())
+
+            credentials {
+                username = providers.gradleProperty("sonatypeUsername").get()
+                password = providers.gradleProperty("sonatypePassword").get()
+            }
+        }
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://maven.scm.adc.com:8081/nexus/content/groups/snapshots/")
+            credentials {
+                username = "swdp"
+                password = "swdp"
+            }
+        }
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://maven.scm.adc.com:8081/nexus/content/repositories/releases/")
+            credentials {
+                username = "swdp"
+                password = "swdp"
+            }
+        }
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -15,6 +43,33 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            //允许使用不安全的maven，gradle7要求必须使用https，内网maven评估不使用https
+            isAllowInsecureProtocol = true
+            //need use oppo maven,due to google repo is not available in services
+            url = uri(providers.gradleProperty("prop_oppoMavenUrlRelease").get())
+
+            credentials {
+                username = providers.gradleProperty("sonatypeUsername").get()
+                password = providers.gradleProperty("sonatypePassword").get()
+            }
+        }
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://maven.scm.adc.com:8081/nexus/content/groups/snapshots/")
+            credentials {
+                username = "swdp"
+                password = "swdp"
+            }
+        }
+        maven {
+            isAllowInsecureProtocol = true
+            url = uri("http://maven.scm.adc.com:8081/nexus/content/repositories/releases/")
+            credentials {
+                username = "swdp"
+                password = "swdp"
+            }
+        }
         google()
         mavenCentral()
     }
